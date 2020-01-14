4a9e5efb660b0a3e1bfcc70b5bb8e66a5883c4c5
Oliver Tan <otan@cockroachlabs.com>
2020-01-14 10:09:33 -0800

    no longer generate too many perms for #38850

--- 38850/delete.py
+++ 38850/delete.py
@@ -1,20 +1,22 @@
 
 permutations = [
-    'ON DELETE NO ACTION',
-    'ON DELETE RESTRICT',
-    'ON DELETE SET NULL',
-    'ON DELETE SET DEFAULT',
-    'ON DELETE CASCADE',
+    ('ON DELETE NO ACTION', 'ON DELETE SET NULL'),
+    ('ON DELETE NO ACTION', 'ON DELETE CASCADE'),
+    ('ON DELETE RESTRICT', 'ON DELETE SET NULL'),
+    ('ON DELETE RESTRICT', 'ON DELETE CASCADE'),
+    ('ON DELETE CASCADE', 'ON DELETE SET DEFAULT'),
+    ('ON DELETE CASCADE', 'ON DELETE SET NULL'),
+    ('ON DELETE SET DEFAULT', 'ON DELETE CASCADE'),
+    ('ON DELETE SET DEFAULT', 'ON DELETE SET NULL'),
+    ('ON DELETE SET NULL', 'ON DELETE SET DEFAULT'),
 ]
 
 print("""statement ok
 CREATE TABLE t1 (a INT PRIMARY KEY); CREATE TABLE t2 (a INT DEFAULT 1)
 """)
 
-for first in permutations:
-    for second in permutations:
-
-        print("""# '{}', followed by '{}'
+for first, second in permutations:
+    print("""# '{}', followed by '{}'
 statement ok
 ALTER TABLE t2 ADD CONSTRAINT fk1 FOREIGN KEY (a) REFERENCES t1 {}; ALTER TABLE t2 ADD CONSTRAINT fk2 FOREIGN KEY (a) REFERENCES t1 {}
 
@@ -22,29 +24,29 @@ statement ok
 insert into t1 values (123); insert into t2 values (123)
 """.format(first, second, first, second))
 
-        expected = "\n123"
+    expected = "\n123"
 
-        # swap to make logic easier.
-        if 'NO ACTION' in first or 'RESTRICT' in first:
-            temp = first
-            first = second
-            second = temp
+    # swap to make logic easier.
+    if 'NO ACTION' in first or 'RESTRICT' in first:
+        temp = first
+        first = second
+        second = temp
 
-        if 'SET DEFAULT' in first:
-            print("statement error value \\[1\\] not found in t1@primary \\[a\\]")
-        elif 'CASCADE' in first or 'SET NULL' in first:
-            if 'CASCADE' in first:
-                expected = ""
-            elif 'SET NULL' in first:
-                expected = "\nNULL"
-            else:
-                assert False, "{}, {}".format(first, second)
-            print("statement ok")
-        elif 'NO ACTION' in second or 'RESTRICT' in second:
-            print('statement error values \\[123\\] in columns \\[a\\] referenced in table "t2"')
+    if 'SET DEFAULT' in first:
+        print("statement error value \\[1\\] not found in t1@primary \\[a\\]")
+    elif 'CASCADE' in first or 'SET NULL' in first:
+        if 'CASCADE' in first:
+            expected = ""
+        elif 'SET NULL' in first:
+            expected = "\nNULL"
         else:
             assert False, "{}, {}".format(first, second)
-        print("""DELETE FROM t1 WHERE a = 123
+        print("statement ok")
+    elif 'NO ACTION' in second or 'RESTRICT' in second:
+        print('123.*referenced.*table "t2"')
+    else:
+        assert False, "{}, {}".format(first, second)
+    print("""DELETE FROM t1 WHERE a = 123
 
 query I
 SELECT * FROM t2
--- 38850/update.py
+++ 38850/update.py
@@ -1,20 +1,21 @@
-
 permutations = [
-    'ON UPDATE NO ACTION',
-    'ON UPDATE RESTRICT',
-    'ON UPDATE SET NULL',
-    'ON UPDATE SET DEFAULT',
-    'ON UPDATE CASCADE',
+    ('ON UPDATE NO ACTION', 'ON UPDATE SET NULL'),
+    ('ON UPDATE NO ACTION', 'ON UPDATE CASCADE'),
+    ('ON UPDATE RESTRICT', 'ON UPDATE SET NULL'),
+    ('ON UPDATE RESTRICT', 'ON UPDATE CASCADE'),
+    ('ON UPDATE CASCADE', 'ON UPDATE SET DEFAULT'),
+    ('ON UPDATE CASCADE', 'ON UPDATE SET NULL'),
+    ('ON UPDATE SET DEFAULT', 'ON UPDATE CASCADE'),
+    ('ON UPDATE SET DEFAULT', 'ON UPDATE SET NULL'),
+    ('ON UPDATE SET NULL', 'ON UPDATE SET DEFAULT'),
 ]
 
 print("""statement ok
 CREATE TABLE t1 (a INT PRIMARY KEY); CREATE TABLE t2 (a INT DEFAULT 1)
 """)
 
-for first in permutations:
-    for second in permutations:
-
-        print("""# '{}', followed by '{}'
+for (first, second) in permutations:
+    print("""# '{}', followed by '{}'
 statement ok
 ALTER TABLE t2 ADD CONSTRAINT fk1 FOREIGN KEY (a) REFERENCES t1 {}; ALTER TABLE t2 ADD CONSTRAINT fk2 FOREIGN KEY (a) REFERENCES t1 {}
 
@@ -22,29 +23,29 @@ statement ok
 insert into t1 values (123); insert into t2 values (123)
 """.format(first, second, first, second))
 
-        expected = "\n123"
+    expected = "\n123"
 
-        # swap to make logic easier.
-        if 'NO ACTION' in first or 'RESTRICT' in first:
-            temp = first
-            first = second
-            second = temp
+    # swap to make logic easier.
+    if 'NO ACTION' in first or 'RESTRICT' in first:
+        temp = first
+        first = second
+        second = temp
 
-        if 'SET DEFAULT' in first:
-            print("statement error value \\[1\\] not found in t1@primary \\[a\\]")
-        elif 'CASCADE' in first or 'SET NULL' in first:
-            if 'CASCADE' in first:
-                expected = "\n2"
-            elif 'SET NULL' in first:
-                expected = "\nNULL"
-            else:
-                assert False, "{}, {}".format(first, second)
-            print("statement ok")
-        elif 'NO ACTION' in second or 'RESTRICT' in second:
-            print('statement error foreign key violation: values \\[123\\] in columns \\[a\\] referenced in table "t2"')
+    if 'SET DEFAULT' in first:
+        print("statement error value \\[1\\] not found in t1@primary \\[a\\]")
+    elif 'CASCADE' in first or 'SET NULL' in first:
+        if 'CASCADE' in first:
+            expected = "\n2"
+        elif 'SET NULL' in first:
+            expected = "\nNULL"
         else:
             assert False, "{}, {}".format(first, second)
-        print("""UPDATE t1 SET a = 2 WHERE a = 123
+        print("statement ok")
+    elif 'NO ACTION' in second or 'RESTRICT' in second:
+        print('statement error 123.*referenced.*table "t2"')
+    else:
+        assert False, "{}, {}".format(first, second)
+    print("""UPDATE t1 SET a = 2 WHERE a = 123
 
 query I
 SELECT * FROM t2
