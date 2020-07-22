7f22eabee243009bfa4e839078540830aea6e59c
Oliver Tan <otan@cockroachlabs.com>
2020-07-22 15:08:30 -0700

    add subway lines

---
+++ geospatial_sql/dump_format/subway_lines.sql
@@ -0,0 +1,6 @@
+CREATE TABLE subway_lines ( route char(1) );
+INSERT INTO subway_lines (route) VALUES
+  ('A'),('B'),('C'),('D'),('E'),('F'),('G'),
+  ('J'),('L'),('M'),('N'),('Q'),('R'),('S'),
+  ('Z'),('1'),('2'),('3'),('4'),('5'),('6'),
+  ('7');
